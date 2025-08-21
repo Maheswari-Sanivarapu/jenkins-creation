48b0cc8936d41e69d3de76f2923f163f404a48ac
maheswari <mahi.sgs22@gmail.com>
2025-08-21 15:07:52 +0530

    jenkins

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    type = string
+    default = "Z07100922S6MUFRUIEXB8"
+    description = "description"
+}
+
+variable "zone_name" {
+    type = string
+    default = "pavithra.fun"
+    description = "description"
+}
